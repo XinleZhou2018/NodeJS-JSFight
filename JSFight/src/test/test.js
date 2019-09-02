e02211899e670a8f2d99fb26412f7bae7bcada9e
XinleZhou2018 <39542071+XinleZhou2018@users.noreply.github.com>
2019-09-02 21:44:21 +0800

    delete original nodejs code

--- JSFight/src/test/test.js
+++
@@ -1,9 +0,0 @@
-const { SuccessModel, ErrorModel } = require('../model/resModel.js');
-
-const successModel = new SuccessModel({data:123}, '成功');
-
-function test(){
-    console.log('result =' + successModel);
-}
-
-module.exports = test;
--- JSFight/src/test/test1.js
+++
@@ -1,7 +0,0 @@
-const test = require('./test.js');
-
-function exec(){
-    test();
-}
-
-module.exports = exec;
