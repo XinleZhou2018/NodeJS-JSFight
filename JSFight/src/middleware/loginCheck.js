8c11525df5d182c0b3069bc1461bbf960dc57946
XinleZhou2018 <39542071+XinleZhou2018@users.noreply.github.com>
2019-09-20 15:26:35 +0800

    修正koa2 登录检查中间件获取小程序header中cookie中的sessionid值

--- JSFight/src/middleware/loginCheck.js
+++ JSFight/src/middleware/loginCheck.js
@@ -1,24 +1,34 @@
 const { ErrorModel } = require('../model/resModel.js')
 const { getRedis } = require('../db/redis.js');
+const ErrorCode = require('../consts/const.js');
+
+const queryString = require("querystring");
 
 
 module.exports = async (ctx, next) =>{
-    if (!ctx.cookies.get('session_id') || ctx.cookies.get('session_id').length <= 0){
-        ctx.body = new ErrorModel(null, ErrorCode.ErrorCode_NotLogin);
-    }else{
-        let session_id = ctx.cookies.get('session_id');
+    console.log(ctx.headers);
 
-        try {
+    console.log(ctx.headers.cookie);
+    try {
+        let cookieValue = queryString.parse(ctx.headers.cookie);
+        if (!cookieValue['session_id'] || cookieValue['session_id'].length <= 0){
+            ctx.body = new ErrorModel(null, ErrorCode.ErrorCode_NotLogin);
+        }else{
+            let session_id = cookieValue['session_id'];
+
             let value = await getRedis(session_id);
-
+    
             if (value == null || value['userid'] == null || value['userid'].length <= 0){
                 ctx.body = new ErrorModel(null, ErrorCode.ErrorCode_NotLogin);
             }else{
                 ctx.userid = value['userid'];
                 await next();
             }
-        } catch (error) {
-            ctx.body = new ErrorModel(error.toString(), ErrorCode.ErrorCode_RedisReadError);
         }
+    } catch (error) {
+        ctx.body = new ErrorModel(error.toString(), ErrorCode.ErrorCode_DefaultError);
     }
+
+    // let cookieValue = queryString.parse(ctx.headers.cookie,';');
+
 };
